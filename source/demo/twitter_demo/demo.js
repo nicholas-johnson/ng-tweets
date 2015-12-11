9b31549ce1b482c1f6a9484699e773629bb6c297
Nicholas Johnson <nicholas@forwardadvance.com>
2015-12-11 12:55:05 +0000

    Demo uses promises

--- source/demo/twitter_demo/demo.js
+++ source/demo/twitter_demo/demo.js
@@ -2,7 +2,7 @@ angular.module('app', ['ngTweets'])
   .controller('twitterController', function($scope, tweets) {
     $scope.tweets = tweets.get({
       widgetId: '598065534836600832'
-    }).success(function(data){
-      $scope.tweets = data;
+    }).then(function(response){
+      $scope.tweets = response.data;
     });
   });
