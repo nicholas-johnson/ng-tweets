fbcf08dc3266684e973db11bad44cd73f7adc097
Eric Gonzalo <ersgonzalo@gmail.com>
2016-03-03 12:39:41 -0500

    Update ng-tweets to be in line with new Twitter widget redesign.
    Twitter Redesign was mentioned here: https://blog.twitter.com/2016/coming-soon-an-improved-embedded-timeline

--- source/js/ng-tweets.js
+++ source/js/ng-tweets.js
@@ -52,29 +52,29 @@ function parse(data) {
   tmp;
 
   if (data.body) {
-    els = angular.element(data.body)[0].getElementsByClassName('tweet');
-    for (x = 0; x < els.length; x++) {
-      el = els[x];
-      tweet = {};
-      tweet.retweet = (el.getElementsByClassName('retweet-credit').length > 0);
-      tweet.id = el.getAttribute('data-tweet-id');
-      tmp = el.getElementsByClassName('e-entry-title')[0];
-      tweet.html = tmp.innerHTML;
-      tweet.text = tmp.textContent || tmp.innerText; // IE8 doesn't support textContent
-      tmp = el.getElementsByClassName('p-author')[0];
-      tweet.author = {
-        url: tmp.getElementsByClassName('profile')[0].getAttribute('href'),
-        avatar: tmp.getElementsByClassName('avatar')[0].getAttribute('src'),
-        fullName: tmp.getElementsByClassName('p-name')[0].innerText,
-        nickName: tmp.getElementsByClassName('p-nickname')[0].innerText
-      };
-      tweet.updated = el.getElementsByClassName('dt-updated')[0].innerText;
-      tweet.permalink = el.getElementsByClassName('permalink')[0].getAttribute('href');
-      if (el.getElementsByClassName('inline-media')[0]) {
-        tweet.inlineMedia = el.getElementsByClassName('inline-media')[0].innerHtml;
+      els = angular.element(data.body)[0].getElementsByClassName('timeline-Tweet');
+      for (x = 0; x < els.length; x++) {
+        el = els[x];
+        tweet = {};
+        tweet.retweet = (el.getElementsByClassName('timeline-Tweet-retweetCredit').length > 0);
+        tweet.id = el.getAttribute('data-tweet-id');
+        tmp = el.getElementsByClassName('timeline-Tweet-text')[0];
+        tweet.html = tmp.innerHTML;
+        tweet.text = tmp.textContent || tmp.innerText; // IE8 doesn't support textContent
+        tmp = el.getElementsByClassName('timeline-Tweet-author')[0];
+        tweet.author = {
+          url: tmp.getElementsByClassName('TweetAuthor-link')[0].getAttribute('href'),
+          avatar: tmp.getElementsByClassName('Avatar')[0].getAttribute('data-src-1x'),
+          fullName: tmp.getElementsByClassName('TweetAuthor-name')[0].innerText,
+          nickName: tmp.getElementsByClassName('TweetAuthor-screenName')[0].innerText
+        };
+        tweet.updated = el.getElementsByClassName('dt-updated')[0].innerText;
+        tweet.permalink = el.getElementsByClassName('timeline-Tweet-timestamp')[0].getAttribute('href');
+        if (el.getElementsByClassName('timeline-Tweet-media')[0]) {
+          tweet.inlineMedia = el.getElementsByClassName('timeline-Tweet-media')[0].innerHTML;
+        }
+        response.tweets.push(tweet);
       }
-      response.tweets.push(tweet);
     }
-  }
   return response;
 }
